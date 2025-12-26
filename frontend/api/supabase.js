6da7049833bbc483baa3df6d36670459e7f64a30
Eman-Aj <emansawesomejourney@gmail.com>
2025-12-26 13:07:59 -0500

    V1.3.21 - Correct Access Of Env Variables
    It was a matter of taking extra time to understand the api intereaction between vercel and the key's hidden in environment variables for API Keys

--- frontend/api/supabase.js
+++ frontend/api/supabase.js
@@ -1,6 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = "https://ynmnytfzjkkucdfavdkc.supabase.co"
-const supabaseKey = "sb_publishable_xBVAdODYXxqWRGg-Xg0pCw_yZyXPZbU"
+
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_KEY
+
 const supabase = createClient(supabaseUrl, supabaseKey) //Getting the url and the key to access the DB API
 
 export default supabase //Public create client where key is still hidden?
